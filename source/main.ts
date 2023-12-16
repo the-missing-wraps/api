c8b43a56b747fbaee81ff6f7d26dde37e40f9270
euberdeveloper <euberdeveloper@gmail.com>
2023-12-16 20:38:04 +0100

    feat: add module alias
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/main.ts
+++ source/main.ts
@@ -1,9 +1,9 @@
 import Fastify from 'fastify';
 import mongoose from 'mongoose';
 import { pino } from 'pino';
-import CONFIG from './config.js';
+import CONFIG from '@/config.js';
 
-import applicationsRoutes from './components/applications/applicationRoutes.js';
+import applicationsRoutes from '@/components/applications/applicationRoutes.js';
 
 try {
     await mongoose.connect(CONFIG.MONGODB.URI);
